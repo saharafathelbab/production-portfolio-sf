1cc91e71affc7cf97a7139d4ffc50a2345d8368e
Sahara <saharafathelbab1@gmail.com>
2026-02-27 16:51:25 -0500

    removing dashes from aria-label for toggle element

--- header/light-dark-toggle/functions/addLightToDarkToggleElements.js
+++ header/light-dark-toggle/functions/addLightToDarkToggleElements.js
@@ -2,7 +2,7 @@ export function addLightToDarkToggleElements() {
     document.getElementById("light-mode-to-dark-mode-toggle").innerHTML =
         `
         <i class="fas fa-solid fa-sun"></i>
-            <button type="button" aria-pressed="false" id="light-dark-toggle" aria-label="light-mode-to-dark-mode-toggle">
+            <button type="button" aria-pressed="false" id="light-dark-toggle" aria-label="Light Mode to Dark Mode Toggle">
                 <i class="fas fa-solid fa-toggle-off"></i>
             </button>
         <i class="fas fa-solid fa-moon"></i>
