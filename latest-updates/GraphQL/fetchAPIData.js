516ea2dd0618561c34fc02a0a33e014636741838
Sahara <saharafathelbab1@gmail.com>
2026-03-05 16:16:43 -0500

    only running fetchAPIData on latest updates page

--- latest-updates/GraphQL/fetchAPIData.js
+++ latest-updates/GraphQL/fetchAPIData.js
@@ -3,39 +3,43 @@ import { addRecentlyReadBooks } from "./helper-functions/addRecentlyReadBooks.js
 
 
 export async function fetchAPIData() {
+  const pageSlug = window.location.pathname;
+
+  if (pageSlug == '/latest-updates') {
     try {
-    // Serverless functions URLs
-    const currentlyReadingEndpointUrl = '/.netlify/functions/getCurrentlyReading';
-    const recentlyReadEndpointUrl = '/.netlify/functions/getRecentlyRead'
+      // Serverless functions URLs
+      const currentlyReadingEndpointUrl = '/.netlify/functions/getCurrentlyReading';
+      const recentlyReadEndpointUrl = '/.netlify/functions/getRecentlyRead'
 
-    // The fetch() function returns a Promise which is fulfilled with a Response object representing the server's response. 
-    const fetchCurrentlyReading = fetch(currentlyReadingEndpointUrl);
-    const fetchRecentlyRead = fetch(recentlyReadEndpointUrl);
+      // The fetch() function returns a Promise which is fulfilled with a Response object representing the server's response. 
+      const fetchCurrentlyReading = fetch(currentlyReadingEndpointUrl);
+      const fetchRecentlyRead = fetch(recentlyReadEndpointUrl);
 
-    // Waiting for both currentlyRead and recentlyRead data to be fetched - this reads the header
-    const [currentlyReadingResponse, recentlyReadResponse] = await Promise.all([fetchCurrentlyReading, fetchRecentlyRead]);
+      // Waiting for both currentlyRead and recentlyRead data to be fetched - this reads the header
+      const [currentlyReadingResponse, recentlyReadResponse] = await Promise.all([fetchCurrentlyReading, fetchRecentlyRead]);
 
-    // Verifying the response came through and if not, the request status
-    if (!currentlyReadingResponse.ok) {
-      throw new Error(`HTTP error! Currently Read status: ${currentlyReadingResponse.status}`);
+      // Verifying the response came through and if not, the request status
+      if (!currentlyReadingResponse.ok) {
+        throw new Error(`HTTP error! Currently Read status: ${currentlyReadingResponse.status}`);
+      }
+
+      if (!recentlyReadResponse.ok) {
+        throw new Error(`HTTP error! Recently Read status: ${currentlyReadingResponse.status}`);
+      }
+
+      // Waiting for both currentlyRead and recentlyRead data - this reads the body
+      const currentlyReadingData = await currentlyReadingResponse.json();
+      // console.log('Data from serverless function:', currentlyReadingData);
+
+      const recentlyReadData = await recentlyReadResponse.json();
+      // console.log('Data from serverless function:', recentlyReadData);
+
+      // Add data to HTML
+      addCurrentlyReadingBooks(currentlyReadingData.data.user_books);
+      addRecentlyReadBooks(recentlyReadData.data.user_books)
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
-
-    if (!recentlyReadResponse.ok) {
-      throw new Error(`HTTP error! Recently Read status: ${currentlyReadingResponse.status}`);
-    }
-
-    // Waiting for both currentlyRead and recentlyRead data - this reads the body
-    const currentlyReadingData = await currentlyReadingResponse.json();
-    // console.log('Data from serverless function:', currentlyReadingData);
-
-    const recentlyReadData = await recentlyReadResponse.json();
-    // console.log('Data from serverless function:', recentlyReadData);
-
-    // Add data to HTML
-    addCurrentlyReadingBooks(currentlyReadingData.data.user_books); 
-    addRecentlyReadBooks(recentlyReadData.data.user_books)
-
-  } catch (error) {
-    console.error('Error fetching data:', error);
   }
 }
